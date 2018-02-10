bb0b39cc3ecc8061a728dd33080d43e6c71539f4
Zakiya Rohman <zakiyarohman8@gmail.com>
2018-02-10 23:39:30 +0530

    Added image preloading

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,19 @@
-$(function(){
+var images = [];
+function preload() {
+    for (var i = 0; i < arguments.length; i++) {
+        images[i] = new Image();
+        images[i].src = preload.arguments[i];
+    }
+}
 
+preload(
+    "https://zakiya123.github.io/profile/Assets/profile-pic.jpg",
+    "https://zakiya123.github.io/profile/Assets/background.jpg"
+)
+
+$(function(){
     setTimeout(function() {
         $("body").addClass("animate");
         
     }, 1000)
-    
-    
-
 });
